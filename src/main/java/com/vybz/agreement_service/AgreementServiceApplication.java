f644fc60fe22fc0c188266d53795bfe0a0d24c05
HyeonProG <guswo3443@naver.com>
2025-06-03 12:38:34 +0900

    first commit

---
+++ src/main/java/com/vybz/agreement_service/AgreementServiceApplication.java
@@ -0,0 +1,13 @@
+package com.vybz.agreement_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgreementServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgreementServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vybz/agreement_service/AgreementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vybz.agreement_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgreementServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
